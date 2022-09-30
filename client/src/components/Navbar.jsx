be085dab145641f7f0197621750936ce9244cf28
Layla Ventillari <laylaventilari5@gmail.com>
2022-09-29 22:39:55 -0300

    style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     return(
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
-                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+                <img src={logo} alt="logo" className="w-70 h-60 cursor-pointer" />
             </div>
             <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
               {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
@@ -38,7 +38,7 @@ const Navbar = () => {
                     >
                         <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
                         {["Market", "Exchange", "Tutorials", "Wallets"].map(
-                            (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
+                            (item, index) => <NavbarItem key={item + index} title={item} classprops="my-2 text-lg" />,
                     )}
                     </ul>
                 )}
